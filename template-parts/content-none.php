a11ec963642e4064205e292fe9acb5ae2e0e3f2b
Shaun Parkison <shacklefurd@gmail.com>
2015-06-30 11:36:56 -0600

    Added col-md-8 class to template parts.

--- template-parts/content-none.php
+++ template-parts/content-none.php
@@ -9,7 +9,7 @@
 
 ?>
 
-<section class="no-results not-found">
+<section class="no-results not-found col-md-8">
 	<header class="page-header">
 		<h1 class="page-title"><?php esc_html_e( 'Nothing Found', 'fullcircle_bootstrap' ); ?></h1>
 	</header><!-- .page-header -->
