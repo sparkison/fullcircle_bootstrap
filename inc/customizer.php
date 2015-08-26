d519f20682373c36ef398016958c5bd5ab09ea8f
Shaun Parkison <shacklefurd@gmail.com>
2015-08-26 13:42:09 -0600

    Moved customizer

--- inc/customizer.php
+++ inc/customizer.php
@@ -21,6 +21,6 @@ add_action( 'customize_register', 'fullcircle_bootstrap_customize_register' );
  * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
  */
 function fullcircle_bootstrap_customize_preview_js() {
-	wp_enqueue_script( 'fullcircle_bootstrap_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20130508', true );
+	wp_enqueue_script( 'fullcircle_bootstrap_customizer', get_template_directory_uri() . '/assets/js/customizer.js', array( 'customize-preview' ), '20130508', true );
 }
 add_action( 'customize_preview_init', 'fullcircle_bootstrap_customize_preview_js' );
